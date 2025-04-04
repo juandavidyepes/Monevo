666f427dfedf856c4a0ea3f576be4610f92e08ad
juandavidyps <judavidy@hotmail.com>
2025-04-04 00:46:56 -0500

    Change to .Net8

--- Program.cs
+++ Program.cs
@@ -9,21 +9,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+// ✅ Usa archivos estáticos desde wwwroot
+app.UseStaticFiles();  
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// ✅ Ruta básica por defecto
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
