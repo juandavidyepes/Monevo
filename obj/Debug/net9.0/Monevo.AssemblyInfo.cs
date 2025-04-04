ac4051f84c45ed242c6a3241222c1404ab923b04
juandavidyps <judavidy@hotmail.com>
2025-04-04 00:17:42 -0500

    Update

--- obj/Debug/net9.0/Monevo.AssemblyInfo.cs
+++ obj/Debug/net9.0/Monevo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monevo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1918d2bd0f0fdb35751f0c7ba99b9e489194a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monevo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monevo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
